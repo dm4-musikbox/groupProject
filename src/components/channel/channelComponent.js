b48b520644d978ea2d681141b83e0eae3107ca29
Theo Beck <eduinfo96@yahoo.com>
2016-11-03 01:27:53 -0500

    finished audio player layout

--- src/components/channel/channelComponent.js
+++ src/components/channel/channelComponent.js
@@ -2,8 +2,23 @@ import channelViewHtml from './channel-view-tmpl.html';
 import './styles/channel.scss'
 
 function channelCtrl() {
-    const channel = this;
 
+  var wavesurfer = WaveSurfer.create({
+  container: '#waveform'
+  , waveColor: '#BDBDBD'
+  , progressColor: '#33312E'
+  , scrollParent: true
+  , hideScrollbar: true
+});
+
+wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+
+wavesurfer.on('ready', function () {
+    wavesurfer.play();
+});
+
+
+    const channel = this;
     channel.test = "This is a test for channel Components!!!";
 
 }
